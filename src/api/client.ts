21584aed6409022739f19338a914dd34a9be1afa
Subhash Varun <86388596+subhash-varun@users.noreply.github.com>
2026-08-01 02:59:26 +0530

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://pgm-backend-latest.onrender.com',
+  baseURL: 'https://pgm-backend-1.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
